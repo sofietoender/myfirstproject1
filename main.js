5817f4505b0bc616242e9b7afaebe7cb260dc162
Sofie Tønder <105886175+sofietoender@users.noreply.github.com>
2022-09-01 11:34:00 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    
+    </a>
+    
+    <h1>Hello Alex 🧠 🤓</h1>
+    <h2> Litt om Alex:<h2>
+    <h3>
+    <li> Kul fyr </li>
+    <li> Ganske smart </li>
+    <li> jobber på Holmlia </li>
+    <li> Han er glad i katter </li>
+    </h3>
+    
+  
+`
+
+
